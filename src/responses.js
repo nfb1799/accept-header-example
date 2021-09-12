3e7ed6060aa204774d8ea8e889da76ef591a3461
nfb1799 <nfb1799@rit.edu>
2021-09-11 21:01:04 -0400

    Added basic API and server code

--- src/responses.js
+++ src/responses.js
@@ -1,4 +1,4 @@
-const fs = require('fs');  // pull in the file system module
+const fs = require('fs'); // pull in the file system module
 
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 
@@ -9,7 +9,28 @@ const respond = (request, response, content, type) => {
 };
 
 const getIndex = (request, response) => {
-  respond(request, response, index, 'text/html');
+  return respond(request, response, index, 'text/html');
+};
+
+const getCats = (request, response, acceptedTypes) => {
+  const cat = {
+    name: "Mr. Meow",
+    age: 5,
+  };
+
+  // xml looks like this: 
+  // <response> <name>Mr. Meow</name> <age>5</age> </response>
+  if(acceptedTypes[0] === 'text/xml') {
+    let responseXML = '<response>';
+    responseXML = `${responseXML} <name>${cat.name}</name>`;
+    responseXML = `${responseXML} <age>${cat.age}</age>`;
+    responseXML = `${responseXML} </response>`;
+
+    return respond(request, response, responseXML, 'text/xml');
+  }
+
+  const catString = JSON.stringify(cat);
+  return respond(request, response, catString, 'application/json');
 };
 
 module.exports = {
--- src/server.js
+++ src/server.js
@@ -5,11 +5,20 @@ const responseHandler = require('./responses.js');
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const urlStruct = {
-
+    '/': responseHandler.getIndex,
+    '/cats': responseHandler.getCats,
+    index: responseHandler.getIndex
 };
 
 const onRequest = (request, response) => {
+    const parsedUrl = url.parse(request.url);
+    const acceptedTypes = request.headers.accept.split(','); //Splits strings at commas (comes in as array)
 
+    if(urlStruct[parsedUrl.pathname]) { //if it exists then true, else false
+        urlStruct[parsedUrl.pathname](request, response, acceptedTypes); //urlStruct[parsedUrl.pathname] is a function
+    } else {
+        urlStruct.index(request, response); //normally, a 404 page would go here
+    }
 };
 
 http.createServer(onRequest).listen(port);
